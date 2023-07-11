cb10d05efad00430ef8a2411dfcefcd44fec7881
Sergius <Sergie.osipchuk@mail.ru>
2023-07-11 19:07:01 +0300

    end jobs.

---
+++ src/HomeWork.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class HomeWork{
+    public static void main(String[] args) {
+        LinkedList<Integer> ls = new LinkedList<>(Arrays.asList(1,2,3,4,5));
+        System.out.println(reverse(ls));
+    }
+    public static LinkedList<Integer> reverse(LinkedList<Integer> ls) {
+        LinkedList<Integer> rev_ls = new LinkedList<>();
+        for(int i=ls.size()-1;i>=0;i--){
+            rev_ls.add(ls.get(i));
+        }
+        return rev_ls;
+
+    }
+}
